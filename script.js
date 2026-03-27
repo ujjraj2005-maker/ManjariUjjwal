2c7daa11890630e743889dd40cbff5562c6c7a42
ujjraj2005-maker <ujjraj2005@gmail.com>
2026-03-27 20:19:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+
+let startDate = new Date("2025-11-21");
+let today = new Date();
+
+let diffTime = today - startDate;
+let days = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+
+document.getElementById("days").innerText = days + " days 💙";
+
+
+let messages = [
+    "Even though we are miles apart, my heart beats only for you ❤️",
+    "I miss you every single day... 😢",
+    "Distance can’t break what we have 💞",
+    "You are always in my thoughts 🌙",
+    "One day, no more distance... only us ❤️"
+];
+
+let msgIndex = 0;
+let charIndex = 0;
+
+function typeEffect() {
+    let currentText = messages[msgIndex];
+
+    if (charIndex < currentText.length) {
+        document.getElementById("typing").innerHTML += currentText.charAt(charIndex);
+        charIndex++;
+        setTimeout(typeEffect, 100);
+    } else {
+        
+        setTimeout(() => {
+            document.getElementById("typing").innerHTML = "";
+            charIndex = 0;
+            msgIndex = (msgIndex + 1) % messages.length;
+            typeEffect();
+        }, 2000);
+    }
+}
+
+function startTyping() {
+    document.getElementById("typing").innerHTML = "";
+    charIndex = 0;
+    typeEffect();
+}
+
+typeEffect();
+
+function showMessage() {
+    let messages = [
+        "No matter the distance, I will always choose you ❤️",
+        "You are my favorite notification 💌",
+        "Distance means nothing when you mean everything 🌍❤️",
+        "I miss you more every day 😢❤️",
+        "Forever us, no matter how far 💫",
+        "I Love You Januuuuuuuu😘😘😘😘😘"
+    ];
+
+    let randomIndex = Math.floor(Math.random() * messages.length);
+
+    document.getElementById("surprise").innerText = messages[randomIndex];
+}
